e666c85a8b7046954c539e5d1ff28ee7e1b8458a
Yuliana Montaño <yulianamarcela200@gmail.com>
2023-06-28 21:23:09 -0400

    first commit

---
+++ src/main/java/com/mycompany/tecnoparcial/TecnoParcial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tecnoparcial;
+
+/**
+ *
+ * @author USER
+ */
+public class TecnoParcial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
