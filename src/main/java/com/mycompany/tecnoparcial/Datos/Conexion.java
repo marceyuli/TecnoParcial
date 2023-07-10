4c03f2f840f36dc5af07426b6abdf0f7a81611bd
Yerireyes <le_soleto@hotmail.com>
2023-07-09 22:39:31 -0400

    Base de datos

---
+++ src/main/java/com/mycompany/tecnoparcial/Datos/Conexion.java
@@ -0,0 +1,76 @@
+package com.mycompany.tecnoparcial.Datos;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Conexion {
+
+    private final String base = "db_grupo01sc";
+    private final String user = "grupo01sc";
+    private final String password = "grup001grup008";
+    private final String url = "jdbc:postgresql://tecnoweb.org.bo:5432/" + base;
+    private final String driver = "org.postgresql.Driver";
+    private Connection con = null;
+
+    public Conexion() {
+    }
+
+    public Connection getConexion() {
+        try {
+            Class.forName(this.driver);
+            con = (Connection) DriverManager.getConnection(this.url, this.user, this.password);
+        } catch (SQLException e) {
+            System.err.println(e);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+
+    public void connect() {
+        try {
+            Class.forName(this.driver);
+            con = DriverManager.getConnection(url, user, password);
+            System.out.println("\033[32;1;2mCONNECTION OPENED!\033[0m");
+        } catch (ClassNotFoundException | SQLException e) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, e);
+            System.err.println("ERROR ON CONNECTING!");
+        }
+    }
+
+    public void close() {
+        try {
+            con.close();
+            System.out.println("\033[32;1;2mCONNECTION CLOSED!!\033[0m");
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("ERROR ON CLOSING!");
+        }
+    }
+
+    public Object query(String query) {
+        Statement statement;
+        Object resultSet = null;
+        String m = query.toLowerCase();
+        boolean type = m.contains("select");
+
+        try {
+            connect();
+            statement = (Statement) con.createStatement();
+
+            if (type) {
+                resultSet = statement.executeQuery(query);
+            } else {
+                resultSet = (statement.executeUpdate(query) >= 0);
+            }
+
+        } catch (SQLException e) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            close();
+        }
+
+        return resultSet;
+    }
+}
---
+++ src/main/java/com/mycompany/tecnoparcial/Datos/Dato.java
@@ -0,0 +1,131 @@
+package com.mycompany.tecnoparcial.Datos;
+
+
+import java.util.regex.*;
+import java.sql.ResultSet;
+
+
+public class Dato {
+
+    protected String TABLE;
+    protected String[] COLUMNS;
+    protected String[] TYPES;
+    protected final Conexion dbc;
+
+    public Dato() {
+        this.dbc = new Conexion();
+    }
+
+    public Dato(String table, String[] Columns) {
+        this.TABLE = table;
+        this.COLUMNS = Columns;
+        this.dbc = new Conexion();
+    }
+
+    private boolean isNumber(Object arg) {
+        String regex = "^(\\d+(\\.\\d+)?)$";
+        return Pattern.matches(regex, String.valueOf(arg));
+    }
+
+    public Tabla listar() {
+        String sql = "SELECT * FROM " + TABLE;
+        return new Tabla((ResultSet) dbc.query(sql));
+    }
+
+    public String[]  getTypesList(){
+        return TYPES;
+    }
+    
+    public String[]  getColums(){
+        return COLUMNS;
+    }
+
+    public Tabla buscarPorID(String id) {
+        String sql = "SELECT * FROM " + TABLE + "WHERE id = " + id;
+        return new Tabla((ResultSet) dbc.query(sql));
+    }
+
+    public Tabla buscar(String[] columnas,Object[] parametros) {
+       
+        String VALUES = "";
+         for (int i = 0; i < COLUMNS.length; i++) {
+            if (!isNumber(parametros[i])) {
+                parametros[i] = "'" + parametros[i] + "'";
+            }
+
+            if (i == COLUMNS.length - 1) {
+                VALUES += columnas[i]+"=%s";
+                
+            } else {
+                VALUES += columnas[i]+"=%s and ";
+               
+            }
+        }
+         
+       String queryString= "SELECT * FROM " + TABLE + " WHERE "+VALUES;
+         String sql = String.format(
+               queryString,
+                parametros
+        );
+
+       
+        return new Tabla((ResultSet) dbc.query(sql));
+    }
+
+    public boolean crear(Object args[]) {
+        String COLS = "";
+        String VALUES = "";
+
+        for (int i = 0; i < COLUMNS.length; i++) {
+            if (!isNumber(args[i])) {
+                args[i] = "'" + args[i] + "'";
+            }
+
+            if (i == COLUMNS.length - 1) {
+                VALUES += "%s";
+                COLS += COLUMNS[i];
+            } else {
+                VALUES += "%s,";
+                COLS += COLUMNS[i] + ",";
+            }
+        }
+
+        String sql = String.format(
+                "INSERT INTO " + TABLE + "(" + COLS + ") VALUES (" + VALUES + ")",
+                args
+        );
+
+        return (boolean) dbc.query(sql);
+    }
+
+    public boolean editar(Object args[]) {
+        String VALUES = "";
+        for (int i = 0; i < COLUMNS.length ; i++) {
+            if (!isNumber(args[i])) {
+                args[i] = "'" + args[i] + "'";
+            }
+         
+            VALUES+=this.COLUMNS[i];
+            VALUES += (i == COLUMNS.length - 1) ? "= %s " : " = %s, ";
+        }
+
+        String sql = String.format(
+                "UPDATE " + TABLE + " SET " + VALUES + "WHERE id = %s",
+                args
+        );
+
+        return (boolean) dbc.query(sql);
+    }
+        
+    public boolean eliminar(String id) {
+        String sql = String.format("DELETE FROM " + TABLE + " WHERE id = %s", id);
+        return (boolean) dbc.query(sql);
+    }
+    public static class Datatypes {
+        public static final String STRING   ="string";
+        public static final String INTEGER   ="integer";
+        public static final String DATE   ="date";
+        public static final String FLOAT   ="float";
+        public static final String TIME   ="time";
+    }
+}
